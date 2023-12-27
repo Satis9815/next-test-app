74e76aeebed296e8aad5d791d42cf414a45540d1
Satis <satis@techxillence.com>
2023-12-27 21:19:56 +0545

    test commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,129 @@
-import Image from 'next/image'
 
+"use client"; 
+import { CiSearch } from 'react-icons/ci';
+import { IoIosNotifications } from 'react-icons/io';
+import { CiSettings } from 'react-icons/ci';
+import { BsThreeDotsVertical } from 'react-icons/bs';
+import { CiClock2 } from 'react-icons/ci';
+import { IoEyeOutline } from 'react-icons/io5';
+import { useState } from 'react';
 export default function Home() {
+
+  const [active, setActive] = useState(false);
+  const toggleDropdown = () => {
+    setActive(!active);
+  };
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="min-h-screen p-2">
+      <header className="mx-auto w-full max-w-7xl flex justify-between mt-4">
+        <div className="relative">
+          <input
+            type="text"
+            placeholder="Search here"
+            className="p-3 outline-0 px-10 rounded-lg text-lg"
+          />
+          <CiSearch size={20} className="absolute top-4 left-3" />
         </div>
-      </div>
+        <nav>
+          <ul className="flex space-x-4 items-center">
+            <li>
+              <IoIosNotifications size={25} />
+            </li>
+            <li>
+              <CiSettings size={25} />
+            </li>
+            <li>
+              <div className="flex items-center space-x-2">
+                <img
+                  className="h-10 w-10 object-cover"
+                  src="https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_640.png"
+                  alt=" "
+                />
+                <div>
+                  <h1 className="text-xl font-semibold">
+                    Satis KumarChaudhary
+                  </h1>
+                  <p className="font-light text-sm">shift Technology admin</p>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <section className="mx-auto w-full max-w-7xl flex justify-between mt-8 p-3 rounded-lg bg-white">
+        <div className="flex justify-between items-center w-full">
+          <div>
+            <h3 className="font-bold text-2xl">My Shifts</h3>
+            <p className="font-light text-sm">Manage Your Shifts</p>
+          </div>
+          <div className="h-9 w-9 bg-slate-300 rounded-full flex items-center justify-center cursor-pointer">
+            <BsThreeDotsVertical size={20} />
+          </div>
+        </div>
+      </section>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <section className="mx-auto w-full max-w-7xl ">
+        <h2 className="mt-3 text-2xl font-bold ">Today&apos;s Shift</h2>
+        <div className=" mt-3 p-3 rounded-lg bg-white flex justify-between">
+          <div className="">
+            <div className="flex space-x-2">
+              <p className="text-sm font-medium">8th jan,2023</p>
+              <p className="text-[10px] text-green-300 rounded-3xl p-1 px-2 bg-yellow-200 ">
+                OPEN
+              </p>
+            </div>
+            <h3 className="font-bold text-2xl my-2">Repair & maintainance</h3>
+            <p className="text-lg font-light">Grand Hyatt melbounce</p>
+          </div>
+          <div className="flex items-center space-x-9 ">
+            <div className="">
+              <div className="flex items-center space-x-2 mb-2">
+                <CiClock2 size={20} />
+                <span className="text-sm">shift time</span>
+              </div>
+              <p className="text-lg font-medium">08:80 - 14:00</p>
+            </div>
+            <div className="">
+              <div className="flex items-center space-x-2 mb-2">
+                <CiClock2 /> <span>Hourly Rate</span>
+              </div>
+              <p className="text-lg font-medium">
+                $20<span className="font-light">/hour</span>
+              </p>
+            </div>
+            <div className="h-full w-[1px] bg-gray-500"></div>
+            <div className="flex items-center flex-col">
+              <p className="text-sm text-green-400 mb-2">
+                Shift starts in 10 mins
+              </p>
+              <div className="relative flex items-center gap-3">
+                <button className="bg-blue-700 text-white font-semibold py-4 px-9 rounded-lg cursor-pointer border-0 outline-none">
+                  Start Shift
+                </button>
+                <div className="h-9 w-9 rounded-lg bg-gray-300 flex items-center cursor-pointer justify-center">
+                  <BsThreeDotsVertical size={20} onClick={toggleDropdown} />
+                </div>
+                {
+                  active && (
+                    <div className="absolute bg-white p-4 right-0 top-14">
+                    {[1, 2, 3, 4].map((item) => (
+                      <div
+                        key={item}
+                        className=" flex items-center gap-1 pb-3 bottom-1 border-b mt-3 last:border-b-0 last:pb-0"
+                      >
+                        <IoEyeOutline size={20} />
+                        <p>View shift Details</p>
+                      </div>
+                    ))}
+                  </div>
+                  )
+                }
+               
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </main>
-  )
+  );
 }
